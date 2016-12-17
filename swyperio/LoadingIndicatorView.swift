396d516c31c0442e173ac5366fe3f890fc875ddc
veganafro <jmuhia80@gmail.com>
2016-12-16 23:23:00 -0500

    start building the loader view

---
+++ swyperio/LoadingIndicatorView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingIndicatorView.swift
+//  swyperio
+//
+//  Created by Jeremia Muhia on 12/16/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+import UIKit
+
+class LoadingIndicatorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
