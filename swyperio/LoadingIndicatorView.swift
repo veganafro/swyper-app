b3dea92ab522d6a55b1639d8641068215298c681
veganafro <jmuhia80@gmail.com>
2016-12-16 23:29:57 -0500

    build loader view

--- swyperio/LoadingIndicatorView.swift
+++ swyperio/LoadingIndicatorView.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class LoadingIndicatorView: UIView {
 
+    let circlePathLayer = CAShapeLayer()
+    let circleRadius: CGFloat = 20.0
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    func configure() {
+    
+        circlePathLayer.frame = bounds
+        circlePathLayer.lineWidth = 2
+        circlePathLayer.fillColor = UIColor.clear.cgColor
+        circlePathLayer.strokeColor = UIColor.red.cgColor
+        
+        layer.addSublayer(circlePathLayer)
+        backgroundColor = UIColor.white
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +43,4 @@ class LoadingIndicatorView: UIView {
         // Drawing code
     }
     */
-
 }
